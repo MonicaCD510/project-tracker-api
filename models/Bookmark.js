c8625e36126c38a7e38ff4715b937e387d31d785
Monica Davila <monica.davila510@gmail.com>
2026-05-23 16:00:55 -0500

    Create bookmark model

---
+++ models/Bookmark.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const bookmarkSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    url: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    notes: {
+      type: String,
+      trim: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Bookmark", bookmarkSchema);
