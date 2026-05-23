c0896bad6974d0762c5c9cae5d06a5b36b6d3460
Monica Davila <monica.davila510@gmail.com>
2026-05-23 15:47:17 -0500

    Connect Express app to MongoDB

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// connect database
+connectDB();
 
 // middleware
 app.use(express.json());
